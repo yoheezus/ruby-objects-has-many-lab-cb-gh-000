3b7d6bb9cd447ca02d02331f9cf91f3424f9fdd2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-15 13:24:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,11 @@ class Song
     end
 
     def artist_name
-        self.artist.name
+        if !self.artist.name.nil?
+            self.artist.name
+        else
+            nil
+        end
     end
 
 end

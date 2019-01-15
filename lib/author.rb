a7d5f4fa973561f247e5f2404883338be604af46
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-15 12:49:21 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,10 +2,14 @@ class Author
 
     attr_accessor :name
 
+    def initialize(name)
+        @name = name
+        @posts = []
+
 
     def add_post_by_title(title)
-
-
+        post = Post.new(title, @name)
+        @posts << post
     end
 
 end

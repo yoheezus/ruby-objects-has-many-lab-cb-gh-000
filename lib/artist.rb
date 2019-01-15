be3df7591531f1455e13612b4b10111dbf8296a5
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-15 13:04:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,28 +2,29 @@ class Artist
 
     attr_accessor :name
 
+    @@all =[]
+
     def initialize(name)
         @name = name
-        @songs = []
     end
 
     def add_song(song)
         song.artist = self
-        @songs << song
+        @all << song
     end
 
     def add_song_by_name(name)
         song = Song.new(name, @name)
-        @songs << song
+        @all << song
 
     end
 
     def songs
-        @songs
+        @all
     end
 
     def self.song_count
-        @songs
+        @all
     end
 
 end
